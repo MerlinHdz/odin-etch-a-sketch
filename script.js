d3530b985e30fb26c6a0662ed557762d2a1e6f96
Merlin Hernandez <83794388+MerlinHdz@users.noreply.github.com>
2026-03-30 12:24:33 -0500

    test commit

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ buttonContainer.appendChild(button);
 
 // prompt user for number, validate input, return integer.
 function getNumberOfSquares() {
-    const input = prompt("Enter number of squares per side for new grid");
+    const input = prompt("Enter number of squares per side for new grid", 16);
     const number = Number(input);
 
     if (Number.isInteger(number) && number > 0 && number < 100) {
@@ -58,12 +58,43 @@ function createGrid(num) {
         square.style.width = `${(CONTAINER_WIDTH / NUMBER_OF_SQUARES)}px`;
 
         // set up event listeners for hovering
-        square.addEventListener("mouseenter", () => {square.classList.add("grid-item-hover")});
+        square.addEventListener("mouseenter", () => {
+            // square.classList.add("grid-item-hover");
+            interatcion(square);
+
+        });
 
 
         container.appendChild(square);
     }
 }
+
+
+// WORK FOR EXTRA CREDIT
+// Create function that will be called every time there is a mouse interaction
+// it should assign a random rgb color to the grid item. With every new interaction, it should darken
+// that color by 10%. After 10 interactions it should be full x color.
+function interatcion(square) {
+
+    // get three random values for rgb color
+    let val1 = Math.floor(Math.random() * 255) + 1;
+    let val2 = Math.floor(Math.random() * 255) + 1;
+    let val3 = Math.floor(Math.random() * 255) + 1;
+
+    // check that background color is not already set.
+    if (!square.style.background) {
+        square.style.background = `rgb(${val1}, ${val2}, ${val3})`;
+        square.style.opacity = '0.1';
+    }
+
+    // increase opacity if it hasn't been maxed out
+    if (+square.style.opacity < 1) {
+        square.style.opacity = `${+square.style.opacity + 0.1}`;
+    }
+
+
+}
+
 
 
 // create initial grid
